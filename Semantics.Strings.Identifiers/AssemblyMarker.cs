77eca52c6be20380fe0bb1454c43e73a97bdf702
Matt Edmondson <matthew.edmondson@gmail.com>
2026-07-01 11:55:38 +1000

    chore(strings): scaffold Semantics.Strings.Identifiers package

---
+++ Semantics.Strings.Identifiers/AssemblyMarker.cs
@@ -0,0 +1,10 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
+namespace ktsu.Semantics.Strings.Identifiers;
+
+/// <summary>Internal marker type; anchors the assembly's root namespace.</summary>
+internal static class AssemblyMarker
+{
+}
